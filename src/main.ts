55f539c84a7e7ae5dde2ce59d50d4e85bdbe99e2
number44 <daniel.lesiewicz.dev@gmail.com>
2024-02-28 15:54:24 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import './style.scss'
+// add classes for mobile navigation toggling
+
+
+window.onload = init
+
+
+import faqScrtipt from './scripts/faq'
+import menuScript from './scripts/menu'
+
+function init(){
+	faqScrtipt()
+	menuScript()	
+}
